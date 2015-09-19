3ac73a5bddb88362209846af160031f91849a8a7
Mayank Gupta <maygupta@C02Q1142G8WM.group.on>
2015-09-19 16:49:55 -0700

    Simple Todo app first commit

---
+++ app/src/main/java/com/groupon/maygupta/todo/MainActivity.java
@@ -0,0 +1,110 @@
+package com.groupon.maygupta.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> itemsAdapter;
+    ListView listItems;
+    EditText textInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        populateArrayItems();
+        listItems = (ListView) findViewById(R.id.lvListItems);
+        listItems.setAdapter(itemsAdapter);
+        textInput = (EditText) findViewById(R.id.etEditText);
+
+        // Creating a long click listener
+        listItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                todoItems.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return false;
+            }
+        });
+    }
+
+    public void populateArrayItems() {
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+
+        // Add file if it doesn't exists
+        if(!file.exists()){
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Assign contents of file to todoItems object
+        try {
+            todoItems = new ArrayList<String>(FileUtils.readLines(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(file, todoItems);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onAddItem(View view) {
+        itemsAdapter.add(textInput.getText().toString());
+        textInput.setText("");
+        writeItems();
+    }
+}
